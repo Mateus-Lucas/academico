ddbd9a71378f6ed6adf93ca1ca10983c09741c0d
Mateus-Lucas <mateus.lucas.ferreira.07@gmail.com>
2023-05-18 21:43:19 -0300

    Intrudoção ao formulário e sua tabela

--- pages/cursos/form.js
+++ pages/cursos/form.js
@@ -1,4 +1,6 @@
 import Pagina from '@/components/Pagina'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 import { Button, Form } from 'react-bootstrap'
 import { useForm } from 'react-hook-form'
@@ -6,32 +8,38 @@ import { useForm } from 'react-hook-form'
 const form = () => {
 
     const { register, handleSubmit } = useForm()
+    const router = useRouter()
 
-    function salvar(dados){
-        console.log(dados);
+    function salvar(dados) {
+        const cursos = JSON.parse(window.localStorage.getItem('cursos')) || []
+        cursos.push(dados)
+        window.localStorage.setItem('cursos', JSON.stringify(cursos))
+        router.push('/cursos/')
     }
 
     return (
         <Pagina titulo='Formulário'>
             <Form>
-                    <Form.Group className="mb-3" controlId='nome'>
-                        <Form.Label >Nome: </Form.Label>
-                        <Form.Control type="text" {...register('nome')}/>
-                    </Form.Group>
+                <Form.Group className="mb-3" controlId='nome'>
+                    <Form.Label >Nome: </Form.Label>
+                    <Form.Control type="text" {...register('nome')} />
+                </Form.Group>
 
-                    <Form.Group className="mb-3" controlId='duracao'>
-                        <Form.Label >Duração: </Form.Label>
-                        <Form.Control type="text" {...register('duracao')}/>
-                    </Form.Group>
+                <Form.Group className="mb-3" controlId='duracao'>
+                    <Form.Label >Duração: </Form.Label>
+                    <Form.Control type="text" {...register('duracao')} />
+                </Form.Group>
 
-                    <Form.Group className="mb-3" controlId='modalidade'>
-                        <Form.Label >Modalidade: </Form.Label>
-                        <Form.Control type="text" {...register('modalidade')}/>
-                    </Form.Group>
+                <Form.Group className="mb-3" controlId='modalidade'>
+                    <Form.Label >Modalidade: </Form.Label>
+                    <Form.Control type="text" {...register('modalidade')} />
+                </Form.Group>
 
+                <Link href='/cursos/'>
                     <Button variant="success" onClick={handleSubmit(salvar)}>
                         Salvar
                     </Button>
+                </Link>
             </Form>
         </Pagina>
 
--- pages/cursos/index.js
+++ pages/cursos/index.js
@@ -1,44 +1,57 @@
 import Pagina from '@/components/Pagina'
-import React from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { Button, Table } from 'react-bootstrap'
-import {AiOutlinePlus} from 'react-icons/ai'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { BsTrash3Fill } from 'react-icons/bs'
 
 const index = () => {
+  
+  const [cursos, setCursos] = useState([])
+
+  useEffect(() => {
+    setCursos(getAll())
+  }, [])
+
+  function getAll(){
+    return JSON.parse(window.localStorage.getItem('cursos')) || []
+  }
+
+  function excluir(id){
+    const itens = getAll()
+    console.log(itens)
+    itens.splice(id, 1)
+    console.log(itens)
+    window.localStorage.setItem('cursos', JSON.stringify(cursos))
+    setCursos(itens)
+  }
+
+
   return (
     <Pagina titulo='Cursos'>
       <Button href='/cursos/form' className='mb-2' variant="primary">Novo
-           <AiOutlinePlus/>
+        <AiOutlinePlus />
       </Button>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Opções</th>
+            <th>Curso</th>
+            <th>Duração</th>
+            <th>Modalidade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {cursos.map((item,lista) => (
+            <tr key={lista}>
+              <td><BsTrash3Fill onClick={()=>excluir(lista)} className='text-danger'/></td>
+              <td>{item.nome}</td>
+              <td>{item.duracao}</td>
+              <td>{item.modalidade}</td>
             </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan={2}>Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-        </Table>
-      </Pagina>
+          ))}
+        </tbody>
+      </Table>
+    </Pagina>
   )
 }
 
